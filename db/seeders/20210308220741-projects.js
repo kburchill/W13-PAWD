2f52d6fc7f48bb0937c2c29d4b9ef4f180952b00
Jim <Jimjnguy@gmail.com>
2021-03-08 18:04:42 -0500

    added project join seeder and tested out query

---
+++ db/seeders/20210308220741-projects.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.bulkInsert(
+			"Projects",
+			[
+				{ name: "onejoe", progress: 10, projectOwnerId: 1, createdAt: new Date(), updatedAt: new Date() },
+				{ name: "twojohn", progress: 10, projectOwnerId: 1, createdAt: new Date(), updatedAt: new Date() },
+				{ name: "twojohn", progress: 10, projectOwnerId: 2, createdAt: new Date(), updatedAt: new Date() },
+			],
+			{}
+		);
+	},
+
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.bulkDelete("Projects", null, {});
+	},
+};
