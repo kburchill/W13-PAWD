a4394b7d65f14025cebc301e996dbd53b6a51fb3
Jim <Jimjnguy@gmail.com>
2021-03-08 15:54:34 -0500

    created Task Migration files and Models, updated relationships

--- db/models/user.js
+++ db/models/user.js
@@ -1,26 +1,30 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    firstName: {
-      allowNull: false,
-      type: DataTypes.STRING(50)
-    },
-    lastName: {
-      allowNull: false,
-      type: DataTypes.STRING(50)
-    },
-    email: {
-      allowNull: false,
-      unique: true,
-      type: DataTypes.STRING(75)
-    },
-    hashedPassword: {
-      allowNull: false,
-      type: DataTypes.STRING.BINARY
-    },
-  }, {});
-  User.associate = function(models) {
-    User.hasMany(models.Project, {foreignKey: "projectOwnerId"})
-  };
-  return User;
+	const User = sequelize.define(
+		"User",
+		{
+			firstName: {
+				allowNull: false,
+				type: DataTypes.STRING(50),
+			},
+			lastName: {
+				allowNull: false,
+				type: DataTypes.STRING(50),
+			},
+			email: {
+				allowNull: false,
+				unique: true,
+				type: DataTypes.STRING(75),
+			},
+			hashedPassword: {
+				allowNull: false,
+				type: DataTypes.STRING.BINARY,
+			},
+		},
+		{}
+	);
+	User.associate = function (models) {
+		User.hasMany(models.Project, { foreignKey: "projectOwnerId" });
+	};
+	return User;
 };
