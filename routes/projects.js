cb3c7eac9bd6741d9ebfc7504741dac46a8d1cd7
Jim <Jimjnguy@gmail.com>
2021-03-09 16:30:45 -0500

    added projects Route Post/ to create projects form. updated Get projects to include existing projects

--- routes/projects.js
+++ routes/projects.js
@@ -1,5 +1,5 @@
 const { Project } = require("../db/models");
-const { Auth } = require("./auth");
+const { restoreUser } = require("../auth");
 const { asyncHandler, csrfProtection } = require("./utils");
 const express = require("express");
 const projectsRouter = express.Router();
@@ -7,27 +7,36 @@ const projectsRouter = express.Router();
 // const { userValidators } = require("./validators");
 
 projectsRouter.get(
-  "/",
-  csrfProtection,
-  asyncHandler(async (req, res) => {
-    const project = await Project.build()
+	"/",
+	csrfProtection,
+	restoreUser,
+	asyncHandler(async (req, res) => {
+		const project = await Project.build();
+		const projects = await Project.findAll();
 
-    res.render("project", {
-      title: "Projects",
-      project,
-      csrfToken: req.csrfToken()
-    })
-  })
-)
+		res.render("project", {
+			title: "Projects",
+			project,
+			projects,
+			csrfToken: req.csrfToken(),
+		});
+	})
+);
 
 projectsRouter.post(
-  "/",
-  csrfProtection,
-  // projectValidators,
-  asyncHandler(async (req, res) => {
-    const { name } = req.body
-    const project = Project.create({ name, progress: 0, projectOwnerId:  });
-  })
-)
+	"/",
+	csrfProtection,
+	restoreUser,
+	// projectValidators,
+	asyncHandler(async (req, res) => {
+		const { projectName } = req.body;
+		console.log(projectName);
+		const {
+			userAuth: { userId },
+		} = req.session;
+
+		const project = await Project.create({ name: projectName, progress: 0, projectOwnerId: userId });
+	})
+);
 
 module.exports = projectsRouter;
