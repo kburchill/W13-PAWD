24bf1d2395743649543fde39bda02a9dbc377dea
Jason Slade <jason.slade906@gmail.com>
2021-03-11 11:10:17 -0700

    ajax create for tasks

--- public/javascripts/tasksAPI.js
+++ public/javascripts/tasksAPI.js
@@ -1,8 +1,9 @@
 import { taskFieldInnerHtml, urlIdIdentifier } from "./api-utils.js";
 
 window.addEventListener("DOMContentLoaded", async () => {
-	const taskTilesContainer = document.querySelector(".tasksContainer");
 
+	// Listener to delete tasks
+	const taskTilesContainer = document.querySelector(".tasksContainer");
 	taskTilesContainer.addEventListener("submit", async (event) => {
 		event.preventDefault();
 
@@ -26,4 +27,32 @@ window.addEventListener("DOMContentLoaded", async () => {
 			console.error("We messed up the tasks api. sorry, jim", err);
 		}
 	});
+
+	//listener for creating a task
+	const taskListForm = document.querySelector(".taskList__form");
+	const taskCreateInput = document.getElementById("createNewTask");
+	taskListForm.addEventListener("submit", async (event) => {
+		event.preventDefault();
+
+		const formData = new FormData(taskListForm);
+		const name = formData.get('name');
+		const priority = formData.get('priority');
+		const projectId = taskListForm.id;
+
+		try{
+			const res = await fetch('/api-tasks', {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ name, priority, projectId })
+			});
+			const tasks = await res.json();
+			taskTilesContainer.innerHTML = "";
+			taskCreateInput.value = "";
+			if (tasks.length) taskFieldInnerHtml(tasks);
+			else return;
+		} catch (err) {
+			console.error("messed up in task creation", err);
+		}
+	});
+
 });
