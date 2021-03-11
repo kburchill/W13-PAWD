f527c48cd5e532512672249e9e03322268672ca1
Kent Burchill <kentburchill@gmail.com>
2021-03-11 14:29:32 -0700

    Added paper style

--- config/database.js
+++ config/database.js
@@ -11,4 +11,9 @@ module.exports = {
     dialect: 'postgres',
     seederStorage: 'sequelize',
   },
+  production: {
+    use_env_variable: 'postgres://kqmwnrfolrprhv:63f6747759dcd2597eab6a4dd6471d46d8103a2373927be5efac7685a87b7d23@ec2-18-214-208-89.compute-1.amazonaws.com:5432/d7pmskmku9fgmhDATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  }
 };
