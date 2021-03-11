636564ad4fc28b56938ff22ddda4e92a9a099244
Jim <Jimjnguy@gmail.com>
2021-03-11 01:32:21 -0500

    added completed boolean to Task.js Model so it can populate on task divs

--- routes/api-tasks.js
+++ routes/api-tasks.js
@@ -9,14 +9,12 @@ apiTaskRouter.delete(
 	requireAuth,
 	asyncHandler(async (req, res) => {
 		const { taskEventId, urlId } = req.body;
-		// console.log(urlId, "URL ID 12 -----------------------------");
-		const currentTask = urlId[1] === "task" ? urlId[0] : null;
-		console.log(currentTask, "==================== CURRENT TASK");
 		const task = await Task.findByPk(taskEventId);
+		const currentTask = urlId[1] === "task" ? urlId[0] : null;
 
 		// REDIRECT BACK TO PROJECT/ID page if you delete current task from inside notes
 		let currentProjectId = await findCurrentProjectId(urlId);
-		console.log(currentProjectId, "CURRENT PROJECTID ----------------------");
+
 		try {
 			await deleteItem(taskEventId, Task);
 		} catch (error) {
