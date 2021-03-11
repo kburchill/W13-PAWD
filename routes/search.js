0c1d1cfd4a978205058b1f0c6302c75d13da4808
Jim <Jimjnguy@gmail.com>
2021-03-11 17:31:58 -0500

    refactored Searching empty results with jason, will skip query and render no results

--- routes/search.js
+++ routes/search.js
@@ -4,38 +4,46 @@ const { Op } = require("sequelize");
 const { Project, Task, Note, User } = require("../db/models");
 const { asyncHandler, deleteItem, findCurrentUser, findCurrentProjectId } = require("./utils");
 const searchRouter = express.Router();
+const { searchValidators, userErrorHandler } = require("./validators");
+const { check, validationResult } = require("express-validator");
 
 searchRouter.post(
 	"/",
 	requireAuth,
 	asyncHandler(async (req, res, next) => {
-		const { searchText } = req.body;
-		const projectOwnerId = findCurrentUser(req.session);
-		// looks for all existing projects with name of search text of current user
-		const allSearchProjects = await Project.findAll({
-			where: {
-				name: { [Op.iLike]: `%${searchText}%` },
-				projectOwnerId,
-			},
-			include: [{ model: Task }],
-		});
-		const allSearchProjectsResults = allSearchProjects.map((project) => project.dataValues);
-		// grabs all existing project ids of the current user
-		const allProjectId = allSearchProjectsResults.map((project) => project.id);
-		// looks for all tasks of the current user with that term in name
-		const allSearchTasks = await Task.findAll({
-			where: {
-				[Op.and]: [
-					{
-						name: { [Op.iLike]: `%${searchText}%` },
-						projectId: allProjectId,
-					},
-				],
-			},
-		});
-		const allSearchTasksResults = allSearchTasks.map((task) => task.dataValues);
+		let { searchText } = req.body;
 
+		let allSearchProjectsResults = [];
+		let allSearchTasksResults = [];
+		const projectOwnerId = findCurrentUser(req.session);
 		const projects = await Project.findAll({ where: { projectOwnerId } });
+
+		if (searchText) {
+			// looks for all existing projects with name of search text of current user
+			const allSearchProjects = await Project.findAll({
+				where: {
+					name: { [Op.iLike]: `%${searchText}%` },
+					projectOwnerId,
+				},
+				include: [{ model: Task }],
+			});
+			allSearchProjectsResults = allSearchProjects.map((project) => project.dataValues);
+			// grabs all existing project ids of the current user
+			const allProjectId = allSearchProjectsResults.map((project) => project.id);
+			// looks for all tasks of the current user with that term in name
+			const allSearchTasks = await Task.findAll({
+				where: {
+					[Op.and]: [
+						{
+							name: { [Op.iLike]: `%${searchText}%` },
+							projectId: allProjectId,
+						},
+					],
+				},
+			});
+			allSearchTasksResults = allSearchTasks.map((task) => task.dataValues);
+		}
+
 		res.render("search", {
 			title: "Search Results",
 			allSearchProjectsResults,
