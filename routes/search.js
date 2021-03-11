5b4b8f4430e19e1fabb08ea9165119ec6ed05a9b
Jim <Jimjnguy@gmail.com>
2021-03-11 15:53:39 -0500

    adding current search

--- routes/search.js
+++ routes/search.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { requireAuth } = require("../auth");
+const { Op } = require("sequelize");
 const { Project, Task, Note, User } = require("../db/models");
 const { asyncHandler, deleteItem, findCurrentUser, findCurrentProjectId } = require("./utils");
 const searchRouter = express.Router();
@@ -9,9 +10,17 @@ searchRouter.post(
 	requireAuth,
 	asyncHandler(async (req, res, next) => {
 		const { searchText } = req.body;
-		const allSearchProjects = await Project.findAll({ where: {} });
+		const allSearchProjects = await Project.findAll();
+		console.log(
+			allSearchProjects.map((project) => {
+				return project.dataValues;
+			})
+		);
+		// const allResults = [allSearchProjects]
 		res.redirect("/projects");
 	})
 );
 
 module.exports = searchRouter;
+
+// { where: { [Op.iLike]: searchText } }
