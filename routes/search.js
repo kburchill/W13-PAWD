6033eceab6e61bc12147dcd39417f8b88ec5e5e4
Jim <Jimjnguy@gmail.com>
2021-03-11 16:51:32 -0500

    added search for task/project functionality

--- routes/search.js
+++ routes/search.js
@@ -10,17 +10,39 @@ searchRouter.post(
 	requireAuth,
 	asyncHandler(async (req, res, next) => {
 		const { searchText } = req.body;
-		const allSearchProjects = await Project.findAll();
-		console.log(
-			allSearchProjects.map((project) => {
-				return project.dataValues;
-			})
-		);
-		// const allResults = [allSearchProjects]
-		res.redirect("/projects");
+		const projectOwnerId = findCurrentUser(req.session);
+		// looks for all existing projects with name of search text of current user
+		const allSearchProjects = await Project.findAll({
+			where: {
+				name: { [Op.iLike]: `%${searchText}%` },
+				projectOwnerId,
+			},
+			include: [{ model: Task }],
+		});
+		const allSearchProjectsResults = allSearchProjects.map((project) => project.dataValues);
+		// grabs all existing project ids of the current user
+		const allProjectId = allSearchProjectsResults.map((project) => project.id);
+		// looks for all tasks of the current user with that term in name
+		const allSearchTasks = await Task.findAll({
+			where: {
+				[Op.and]: [
+					{
+						name: { [Op.iLike]: `%${searchText}%` },
+						projectId: allProjectId,
+					},
+				],
+			},
+		});
+		const allSearchTasksResults = allSearchTasks.map((task) => task.dataValues);
+
+		const projects = await Project.findAll({ where: { projectOwnerId } });
+		res.render("search", {
+			title: "Search Results",
+			allSearchProjectsResults,
+			allSearchTasksResults,
+			projects,
+		});
 	})
 );
 
 module.exports = searchRouter;
-
-// { where: { [Op.iLike]: searchText } }
