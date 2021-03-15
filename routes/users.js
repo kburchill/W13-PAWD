7f4ec6b6b634739737f3ed2fc28096f532462412
Jim <Jimjnguy@gmail.com>
2021-03-15 02:43:40 -0400

    added login validator logic

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { User } = require("../db/models");
 const { asyncHandler, csrfProtection } = require("./utils");
 const { userValidators, loginValidators, userErrorHandler } = require("./validators");
-const bcrypt = require('bcryptjs');
-const { loginUser, logoutUser } = require('../auth');
+const bcrypt = require("bcryptjs");
+const { loginUser, logoutUser } = require("../auth");
 
 /* GET users listing. */
 router.get(
@@ -32,12 +32,12 @@ router.post(
 		const { errors } = req.body;
 
 		if (req.body.errors) {
-			res.render('sign-up', {
-				title: 'sign-up',
+			res.render("sign-up", {
+				title: "sign-up",
 				csrfToken: req.csrfToken(),
 				errors,
-				user: req.body
-			})
+				user: req.body,
+			});
 		} else {
 			const { firstName, lastName, email, password } = req.body;
 			const hashedPassword = await bcrypt.hash(password, 10);
@@ -47,71 +47,82 @@ router.post(
 
 			return req.session.save((e) => {
 				if (e) return next(e);
-				return res.redirect('/projects')
+				return res.redirect("/projects");
 			});
 		}
 	})
 );
 
-router.post('/logout', (req, res) => {
+router.post("/logout", (req, res) => {
 	logoutUser(req, res);
-	res.redirect('/users/login')
-})
+	res.redirect("/users/login");
+});
 
-router.get('/login', csrfProtection, (req, res) => {
+router.get("/login", csrfProtection, (req, res) => {
 	const errors = false;
 	const user = User.build();
 
-	res.render('login', {
+	res.render("login", {
 		title: "Login",
 		errors,
 		user,
 		csrfToken: req.csrfToken(),
-	})
-})
+	});
+});
 
-router.post('/login',
+router.post(
+	"/login",
 	csrfProtection,
 	loginValidators,
 	userErrorHandler,
 	asyncHandler(async (req, res, next) => {
 		const { errors } = req.body;
 		if (req.body.errors) {
-			const user = User.build()
-			res.render('login', {
-				title: 'login',
+			const user = User.build();
+			res.render("login", {
+				title: "login",
 				csrfToken: req.csrfToken(),
 				errors,
-				user: req.body
+				user: req.body,
 				// errors: req.body.errors
-			})
+			});
 		} else {
-		const { email, password } = req.body
+			const { email, password } = req.body;
 
-		try {
-			const user = await User.findOne({ where: { email } })
-			const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+			try {
+				let user = await User.findOne({ where: { email } });
+				const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+				user = { email };
 
-			if (passwordMatch) {
-				loginUser(req, res, user)
-				res.redirect('/projects')
-			} else {
-				throw new Error("Invalid credentials")
+				if (passwordMatch) {
+					loginUser(req, res, user);
+					res.redirect("/projects");
+				} else {
+					res.render("login", {
+						title: "Login",
+						user,
+						errors: { password: "Invalid credentials" },
+						csrfToken: req.csrfToken(),
+					});
+				}
+			} catch (err) {
+				res.render("login", {
+					title: "Login",
+					errors: ["Invalid credentials"],
+					csrfToken: req.csrfToken(),
+				});
 			}
-		} catch (err) {
-			res.render('login', {
-				title: "Login",
-				errors: ["Invalid credentials"],
-				csrfToken: req.csrfToken(),
-			})
 		}
-	}
-	}))
+	})
+);
 
-	router.post('/demo', asyncHandler(async (req, res) => {
-		const user = { id: 1 }
+router.post(
+	"/demo",
+	asyncHandler(async (req, res) => {
+		const user = { id: 1 };
 		loginUser(req, res, user);
-		res.redirect('/projects');
-	}))
+		res.redirect("/projects");
+	})
+);
 
 module.exports = router;
